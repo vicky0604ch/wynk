8fd02756cc05d5d1df29d97db4448e08dca01445
vicky0604hello <166818922+vicky0604hello@users.noreply.github.com>
2024-05-26 22:38:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,20 +11,20 @@ API_HASH = getenv("API_HASH","4e984ea35f854762dcde906dce426c2d")
 BOT_TOKEN = getenv("BOT_TOKEN","")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "9999"))
-LOGGER_ID = int(getenv("LOGGER_ID", "-1001971806089"))
-BOTADDLOGS = int(getenv("BOTADDLOGS", "-1001971806089")) # LOGGER_ID Id Also Use No Problem
-GBAN_LOGS = int(getenv("GBAN_LOGS", "-1002046393302"))
-GCAST_USERS = list(map(int, getenv("GCAST_USERS", "2105971379 7137269276 7045191057").split()))
-OWNER_ID = int(getenv("OWNER_ID", 7137269276))
-OWNER = int(getenv("OWNER", 2105971379))
-OWNER_USERNAME = getenv("OWNER_USERNAME","sultan11100")
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002136795173"))
+BOTADDLOGS = int(getenv("BOTADDLOGS", "-1002136795173")) # LOGGER_ID Id Also Use No Problem
+GBAN_LOGS = int(getenv("GBAN_LOGS", "-1002136795173"))
+GCAST_USERS = list(map(int, getenv("GCAST_USERS", "7142645781").split()))
+OWNER_ID = int(getenv("OWNER_ID", 7142645781))
+OWNER = int(getenv("OWNER", 7142645781))
+OWNER_USERNAME = getenv("OWNER_USERNAME","VP_1203")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/AbhiModszYT/AnieXEricaMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/vicky0604hello/guppshupp")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN",None)
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Logs_Gban")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AM_YTSUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/botverse_community")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/CNC_GROUPS")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", 7000))
@@ -56,18 +56,18 @@ AMBOT = [
     "ᴘʀᴏᴄᴇꜱꜱɪɴɢ..",
 ]
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = getenv("STATS_IMG_URL", "https://graph.org/file/f234fa4e140eb1b85d185.jpg")
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/9405aafe6384e21b46af5.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/9405aafe6384e21b46af5.jpg")
+PLAYLIST_IMG_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
+STATS_IMG_URL = getenv("STATS_IMG_URL", "https://graph.org/file/9405aafe6384e21b46af5.jpg")
+TELEGRAM_AUDIO_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
+STREAM_IMG_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/9405aafe6384e21b46af5.jpg"
 
 
 def time_to_seconds(time):
